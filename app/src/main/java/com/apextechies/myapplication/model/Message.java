9c0ba7cd3d057a85307d2561192db7d6beeccbbf
shankar-spotsoon <shankar@spotsoon.com>
2018-04-26 23:28:29 +0530

    first commit

---
+++ app/src/main/java/com/apextechies/myapplication/model/Message.java
@@ -0,0 +1,10 @@
+package com.apextechies.myapplication.model;
+
+
+
+public class Message{
+    public String idSender;
+    public String idReceiver;
+    public String text;
+    public long timestamp;
+}
---
+++ app/src/main/java/com/apextechies/myapplication/model/Status.java
@@ -0,0 +1,13 @@
+package com.apextechies.myapplication.model;
+
+
+
+public class Status{
+    public boolean isOnline;
+    public long timestamp;
+
+    public Status(){
+        isOnline = false;
+        timestamp = 0;
+    }
+}
